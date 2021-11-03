73f3b61d9cd8baead95c9618f1ef5a1ef55b5edf
mrspaghettiiscool <Jonahposner@icloud.com>
2021-11-03 20:51:10 +0000

    start argparse development

---
+++ pyBase64args.py
@@ -0,0 +1,33 @@
+import argparse
+import base64
+
+import pdb; pdb.set_trace()
+
+parser = argparse.ArgumentParser(description='Encode and decode base64.')
+parser.add_argument('-e', dest='accumulate',
+                    help='encode provided text into base64.')
+parser.parse_args()
+
+
+choice = input("Would you like to encode or decode? ")
+
+def base64encode(toencode):
+    done = []
+    done.append(base64.b64encode(bytes(toencode, 'utf_8')))
+    return done
+
+def base64decode(todecode):
+    done = []
+    done.append(base64.b64decode(bytes(todecode, 'utf_8')))
+    return done
+
+if choice.lower() == 'encode':
+    en = input("What would you like to encode? ")
+    print(base64encode(en))
+
+elif choice.lower() == 'decode':
+    de = input("What would you like to decode? Include the padding. ")
+    print(base64decode(de))
+
+else:
+    print("I can't do that")
