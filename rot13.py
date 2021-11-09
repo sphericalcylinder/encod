1d14a3d530375a26830620cd73f447a064981231
mrspaghettiiscool <jonahposner@icloud.com>
2021-11-09 18:27:31 -0500

    very quick, slide-it-in, super sleuth rot13 argument support

--- rot13.py
+++ rot13.py
@@ -1,5 +1,11 @@
 import codecs
-choice = input("Do you want to encode or decode? ")
+import sys
+from signature import Signature
+
+signature = Signature()
+signature.sign()
+
+err_msg = "You must provide an input after the argument"
 
 def rotencode(stuffToEncode):
     res = codecs.encode(stuffToEncode, 'rot_13')
@@ -9,13 +15,43 @@ def rotdecode(encodedStuff):
     res = codecs.decode(encodedStuff, 'rot_13')
     return res
 
-if choice.lower() == "encode":
-    en = input("What would you like to encode? ")
-    print(rotencode(en))
+def noArgs():
 
-elif choice.lower() == "decode":
-    de = input("What would you like to decode? ")
-    print(rotdecode(de))
+    choice = input("Do you want to encrypt or decode? ")
+
+    if choice.lower() == "encode":
+        en = input("What would you like to encrypt? ")
+        print(rotencode(en))
+        sys.exit(0)
+
+    elif choice.lower() == "decrypt":
+        de = input("What would you like to decrypt? ")
+        print(rotdecode(de))
+        sys.exit(0)
+
+    else:
+        print("Sorry, I can't do that")
+        sys.exit(0)
+
+if len(sys.argv) < 2:
+    noArgs()
+
+if sys.argv[1] == '-e' or sys.argv[1] == '--encrypt':
+    try:
+        print(rotencode(sys.argv[2]))
+    except:
+        print(err_msg)
+
+elif sys.argv[1] == '-d' or sys.argv[1] == '--decrypt':
+    try:
+        print(rotdecode(sys.argv[2]))
+    except:
+        print(err_msg)
+
+elif sys.argv[1] == '-h' or sys.argv[1] == '--help':
+    print("""Use:
+    encode(-e, --encrypt) or decode(-d, --decrypt) ROT13.""")
 
 else:
-    print("Sorry, I can't do that")
+    print("Unknown first argument: %s" % sys.argv[1])
+    print("-h or --help for more help")
